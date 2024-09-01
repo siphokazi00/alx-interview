2e774996221fe88563ef820367c8f655570003e6
Siphokazi <usphow@gmail.com>
2024-09-01 11:21:37 -0700

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Calculate the fewest no. of operations needed to result in n H chars in a file.
+"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
