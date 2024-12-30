640b95b4d6d3c4420f409972e94e00e013a0b872
siphokazi <usphow@gmail.com>
2024-12-30 15:31:36 +0000

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 
-// Get the movie ID from the first command-line argument
 const movieID = process.argv[2];
 
-// Define the URL for the Star Wars API films endpoint
-const apiUrl = `https://swapi.dev/api/films/${movieID}/`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
 
-// Function to fetch and display the characters of the movie
 function fetchMovieCharacters() {
-  // Make a request to the Star Wars API for the specific movie
   request(apiUrl, (error, response, body) => {
     if (error) {
       console.error('Error fetching the movie:', error);
@@ -21,13 +17,10 @@ function fetchMovieCharacters() {
       return;
     }
 
-    // Parse the movie data from the API response
     const movieData = JSON.parse(body);
 
-    // Get the characters array from the movie data
     const characters = movieData.characters;
 
-    // Loop through the characters and fetch each one by URL
     characters.forEach((characterUrl) => {
       request(characterUrl, (error, response, body) => {
         if (error) {
@@ -37,7 +30,6 @@ function fetchMovieCharacters() {
 
         if (response.statusCode === 200) {
           const characterData = JSON.parse(body);
-          // Print the character name
           console.log(characterData.name);
         }
       });
@@ -45,6 +37,4 @@ function fetchMovieCharacters() {
   });
 }
 
-// Execute the function to fetch and display characters
 fetchMovieCharacters();
-
